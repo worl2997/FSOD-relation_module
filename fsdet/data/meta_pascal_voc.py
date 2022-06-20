9811bed1b087ff240c1bbd1af26e3b16d299f567
worl2997 <worl2997@naver.com>
2022-06-20 23:37:55 +0900

    Initial commit

---
+++ fsdet/data/meta_pascal_voc.py
@@ -0,0 +1,177 @@
+import os
+import xml.etree.ElementTree as ET
+
+import numpy as np
+import torch
+from detectron2.data import DatasetCatalog, MetadataCatalog
+from detectron2.structures import BoxMode
+from fsdet.utils.file_io import PathManager
+import pickle
+__all__ = ["register_meta_pascal_voc"]
+
+# 커스텀 데이로더 정의
+def load_filtered_voc_instances(name: str, dirname: str, split: str, classnames: str):
+    """
+    Load Pascal VOC detection annotations to Detectron2 format.
+    Args:
+        dirname: Contain "Annotations", "ImageSets", "JPEGImages"
+        split (str): one of "train", "test", "val", "trainval"
+
+    name :  "voc_{}_trainval_{}{}_{}shot{}"
+    dirname :  root : datasets , dirname: VOC2007 / VOC2012
+    split: {}_{}shot_split_{}_trainval  ex) all_1shot_split_1_trainval, novel_1shot_split_1_trainval
+    classnames : 정의된 base 및 novel 클래스 목록
+    """
+
+    # name -> "voc_{}_trainval_{}{}_{}shot{}
+    # few-shot 일경우
+    is_shots = "shot" in name
+    if is_shots:
+        fileids = {}
+        split_dir = os.path.join("datasets", "vocsplit")
+        if "seed" in name:
+            shot = name.split("_")[-2].split("shot")[0]  #
+            seed = int(name.split("_seed")[-1])
+            split_dir = os.path.join(split_dir, "seed{}".format(seed))
+        else:
+            shot = name.split("_")[-1].split("shot")[0]
+        for cls in classnames:
+            with PathManager.open(
+                os.path.join(
+                    split_dir, "box_{}shot_{}_train.txt".format(shot, cls)
+                )
+            ) as f:
+                fileids_ = np.loadtxt(f, dtype=np.str).tolist()
+                if isinstance(fileids_, str):
+                    fileids_ = [fileids_]
+                fileids_ = [
+                    fid.split("/")[-1].split(".jpg")[0] for fid in fileids_
+                ]
+                fileids[cls] = fileids_
+
+    else:
+        # base data
+        with PathManager.open(os.path.join(dirname, "ImageSets", "Main", split + ".txt")) as f:
+            fileids = np.loadtxt(f, dtype=np.str)
+
+
+    dicts = []
+
+    # 새로 추가한 glove data
+    glove_path = "/home/cclab/바탕화면/ICTC_paper_jacky/few-shot-object-detection-master-relation_module/datasets/word2vec_base_split1.pkl"
+    with open(glove_path, 'rb') as f:
+        voc_glove = pickle.load(f)
+        f.close()
+    #voc_glove = torch.Tensor(voc_glove)
+
+    if is_shots:
+        for cls, fileids_ in fileids.items():
+
+            dicts_ = []
+            for fileid in fileids_:
+                year = "2012" if "_" in fileid else "2007"
+                dirname = os.path.join("datasets", "VOC{}".format(year))
+                anno_file = os.path.join(
+                    dirname, "Annotations", fileid + ".xml"
+                )
+                jpeg_file = os.path.join(
+                    dirname, "JPEGImages", fileid + ".jpg"
+                )
+
+                tree = ET.parse(anno_file)
+
+                for obj in tree.findall("object"):
+                    r = {
+                        "file_name": jpeg_file,
+                        "image_id": fileid,
+                        "height": int(tree.findall("./size/height")[0].text),
+                        "width": int(tree.findall("./size/width")[0].text),
+                        "voc_glove": voc_glove  # 추가된 부분
+
+                    }
+                    cls_ = obj.find("name").text
+                    if cls != cls_:
+                        continue
+                    bbox = obj.find("bndbox")
+                    bbox = [
+                        float(bbox.find(x).text)
+                        for x in ["xmin", "ymin", "xmax", "ymax"]
+                    ]
+                    bbox[0] -= 1.0
+                    bbox[1] -= 1.0
+
+                    instances = [
+                        {
+                            "category_id": classnames.index(cls),
+                            "bbox": bbox,
+                            "bbox_mode": BoxMode.XYXY_ABS,
+                            # "glove": 데이터 로드하는 코드 추가
+                        }
+                    ]
+                    r["annotations"] = instances
+                    dicts_.append(r)
+
+
+            if len(dicts_) > int(shot):
+                dicts_ = np.random.choice(dicts_, int(shot), replace=False)
+            dicts.extend(dicts_)
+
+    else:
+        # base trainig 에서는 Glove 데이터를 뻄
+        for fileid in fileids:
+            anno_file = os.path.join(dirname, "Annotations", fileid + ".xml")
+            jpeg_file = os.path.join(dirname, "JPEGImages", fileid + ".jpg")
+
+            tree = ET.parse(anno_file)
+
+            r = {
+                "file_name": jpeg_file,
+                "image_id": fileid,
+                "height": int(tree.findall("./size/height")[0].text),
+                "width": int(tree.findall("./size/width")[0].text),
+                "voc_glove" : voc_glove # 추가된 부분
+            }
+            instances = []
+
+            for obj in tree.findall("object"):
+                cls = obj.find("name").text
+                if not (cls in classnames):
+                    continue
+                bbox = obj.find("bndbox")
+                bbox = [
+                    float(bbox.find(x).text)
+                    for x in ["xmin", "ymin", "xmax", "ymax"]
+                ]
+                bbox[0] -= 1.0
+                bbox[1] -= 1.0
+
+                instances.append(
+                    {
+                        "category_id": classnames.index(cls),
+                        "bbox": bbox,
+                        "bbox_mode": BoxMode.XYXY_ABS,
+                    }
+                )
+            r["annotations"] = instances
+            dicts.append(r)
+    return dicts
+
+
+def register_meta_pascal_voc(name, metadata, dirname, split, year, keepclasses, sid):
+    if keepclasses.startswith("base_novel"):
+        thing_classes = metadata["thing_classes"][sid]
+    elif keepclasses.startswith("base"):
+        thing_classes = metadata["base_classes"][sid]
+    elif keepclasses.startswith("novel"):
+        thing_classes = metadata["novel_classes"][sid]
+
+    DatasetCatalog.register(name, lambda: load_filtered_voc_instances(name, dirname, split, thing_classes),)
+
+    MetadataCatalog.get(name).set(
+        thing_classes=thing_classes,
+        dirname=dirname,
+        year=year,
+        split=split,
+        base_classes=metadata["base_classes"][sid],
+        novel_classes=metadata["novel_classes"][sid]
+    )
---
+++ tools/train_net.py
@@ -0,0 +1,118 @@
+"""
+Detection Training Script.
+
+이 스크립트는 지정된 구성 파일을 읽고 교육 또는 평가를 실행합니다.
+FsDet에서 표준 모델을 교육하기 위해 만들어진 entry point이다 .
+
+하나의 스크립트가 여러 모델의 교육을 지원하도록 하기 위해 이 스크립트는 이러한 기본 제공 모델에
+고유한 논리를 포함하므로 자체 프로젝트에 적합하지 않을 수 있습니다.
+예를 들어, 연구 프로젝트에는 단일 "평가자"만 필요할 수 있습니다.
+
+따라서 FsDet을 라이브러리로 사용하고 이 파일을 라이브러리 사용 방법의 예로 사용하는 것이 좋습니다.
+데이터 세트 및 기타 사용자 지정이 포함된 스크립트를 직접 작성할 수 있습니다.
+"""
+
+import os
+
+import detectron2.utils.comm as comm
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.data import MetadataCatalog
+from detectron2.engine import launch
+
+from fsdet.config import get_cfg, set_global_cfg
+from fsdet.engine import DefaultTrainer, default_argument_parser, default_setup
+from fsdet.evaluation import (
+    COCOEvaluator,
+    DatasetEvaluators,
+    LVISEvaluator,
+    PascalVOCDetectionEvaluator,
+    verify_results,
+)
+
+
+class Trainer(DefaultTrainer):
+    """
+    We use the "DefaultTrainer" which contains a number pre-defined logic for
+    standard training workflow. They may not work for you, especially if you
+    are working on a new research project. In that case you can use the cleaner
+    "SimpleTrainer", or write your own training loop.
+    """
+
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+        """
+        Create evaluator(s) for a given dataset.
+        This uses the special metadata "evaluator_type" associated with each builtin dataset.
+        For your own dataset, you can simply create an evaluator manually in your
+        script and do not have to worry about the hacky if-else logic here.
+        """
+        if output_folder is None:
+            output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
+        evaluator_list = []
+        evaluator_type = MetadataCatalog.get(dataset_name).evaluator_type
+        if evaluator_type == "coco":
+            evaluator_list.append(
+                COCOEvaluator(dataset_name, cfg, True, output_folder)
+            )
+        if evaluator_type == "pascal_voc":
+            return PascalVOCDetectionEvaluator(dataset_name)
+        if evaluator_type == "lvis":
+            return LVISEvaluator(dataset_name, cfg, True, output_folder)
+        if len(evaluator_list) == 0:
+            raise NotImplementedError(
+                "no Evaluator for the dataset {} with the type {}".format(
+                    dataset_name, evaluator_type
+                )
+            )
+        if len(evaluator_list) == 1:
+            return evaluator_list[0]
+        return DatasetEvaluators(evaluator_list)
+
+
+def setup(args):
+    """
+    Create configs and perform basic setups.
+    """
+    cfg = get_cfg()
+    cfg.merge_from_file(args.config_file)
+    if args.opts:
+        cfg.merge_from_list(args.opts)
+    cfg.freeze()
+    set_global_cfg(cfg)
+    default_setup(cfg, args)
+    return cfg
+
+
+def main(args):
+    cfg = setup(args)
+
+    if args.eval_only:
+        model = Trainer.build_model(cfg)
+        DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
+            cfg.MODEL.WEIGHTS, resume=args.resume
+        )
+        res = Trainer.test(cfg, model)
+        if comm.is_main_process():
+            verify_results(cfg, res)
+        return res
+
+    """
+    If you'd like to do anything fancier than the standard training logic,
+    consider writing your own training loop or subclassing the trainer.
+    """
+    trainer = Trainer(cfg)
+    trainer.resume_or_load(resume=args.resume)
+    return trainer.train()
+
+
+if __name__ == "__main__":
+    args = default_argument_parser().parse_args()
+    print("Command Line Args:", args)
+    launch(
+        main,
+        args.num_gpus,
+        num_machines=args.num_machines,
+        machine_rank=args.machine_rank,
+        dist_url=args.dist_url,
+        args=(args,),
+    )
